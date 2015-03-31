818d7afb75213fe3e29855282e2358620e7a18e5
igorravasi <igorravasi@gmail.com>
2015-03-31 15:41:04 +0200

    Igor On

--- src/server/IlMain.java
+++ src/server/IlMain.java
@@ -5,7 +5,8 @@ public class IlMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Prova SVN ");
+		System.out.println("Prova SVN  ");
+		System.out.println("igorravasi On");
 		System.out.println(" ScrumBoy ON ");
 		System.out.println(" ghia16 ON ");
 	}
