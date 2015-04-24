0a6f3851c96ec545a5eaada414b9ba77f5197f46
igorravasi <igorravasi@gmail.com>
2015-04-25 00:04:11 +0200

    Rinominato da menu refactoring IlMain in ServerLife

--- src/server/ServerLife.java
+++ src/server/ServerLife.java
@@ -4,19 +4,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class IlMain {
+public class ServerLife {
 
 	public static void main(String[] args) {
 		
 		
-		// TODO Auto-generated method stub
 
-//		System.out.println("Prova SVN  ");
-//		System.out.println("igorravasi On");
-//		System.out.println(" ScrumBoy ON ");
-//		System.out.println(" ghia16 ON ");
-//		System.out.println(" bipavesi ON ");
-//		System.out.println("edoardo OFF");
 		
 		try {
 			ServerSocket socket=new ServerSocket(ServerParameters.PORT);
