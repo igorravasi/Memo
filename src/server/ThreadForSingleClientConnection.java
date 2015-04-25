825f86fff7536f99391c7f91287fc5121d322be2
igorravasi <igorravasi@gmail.com>
2015-04-25 21:16:21 +0200

    Migliorata la gestione della comunicazione con il client

--- src/server/ThreadForSingleClientConnection.java
+++ src/server/ThreadForSingleClientConnection.java
@@ -8,13 +8,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.nio.charset.Charset;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class ThreadForSingleClientConnection extends Thread implements Runnable{
 
 	private Socket clientSocket;
 	private Integer id;
+	
+	@SuppressWarnings("serial")
+	
+	private class BadStringException extends Exception{
+
+	}
 	
 	public ThreadForSingleClientConnection(Socket clientSocket) {
 		super();
@@ -31,61 +36,88 @@ public class ThreadForSingleClientConnection extends Thread implements Runnable{
 	}
 
 
-
-	
-
 	@Override
 	public void run() {
 
-			
+	
 		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			String inputRequest =  in.readLine();
-			if (inputRequest == null) {
-				throw new Exception("not input lines");
-			}
-			String line = "\n";
 			
-			while(line != null && line.length() != 0){
-				inputRequest += line + "\n";
-				line = in.readLine();
-				
-			}
+			String inputRequest = getRequestLines();
 			
 			//Riga di debug
 			System.out.println(id +  "--> \n" + inputRequest);
 			
-			StringTokenizer tokenizer = new StringTokenizer(inputRequest);
-			tokenizer.nextToken();
-			if (tokenizer.nextToken().compareToIgnoreCase("/singleplayer") == 0) {
-				writeSequence();
-			}
+			handleRequest(inputRequest);
+			
+			clientSocket.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (BadStringException e) {
+//			Non fare niente e continua (chiude il thread) senza fermare l'esecuzione del programma
+		}
 
 		
-		} catch (Exception e) {
-			//Non fare niente e termina il thread
+	}
+
+
+	private String getRequestLines() throws IOException, BadStringException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String inputRequest =  in.readLine();
+		if (inputRequest == null) {
+			throw new BadStringException();
+			
+		}
+		String line = "\n";
+		
+		while(line != null && line.length() != 0){
+			inputRequest += line + "\n";
+			line = in.readLine();
+			
+		}
+		return inputRequest;
+	}
+
+// TODO: gestire richieste di file non html (esempio immagini)
+	
+	private void handleRequest(String inputRequest)throws FileNotFoundException, IOException {
+		
+		
+		StringTokenizer tokenizer = new StringTokenizer(inputRequest);
+		tokenizer.nextToken();
+		String page = tokenizer.nextToken();
+		
+		if (page == null) {
+//			TODO: error
+			page = "/error";
 		}
 		
+		
+		if (page.compareToIgnoreCase("/singleplayer") == 0) {
+			writeSequence();
+		} else if (page.compareToIgnoreCase("/error") == 0) {
+			writeOutputMessage("Si è verificato un errore");
+		
+		} else {
+			writeOutputFromFile(page);
+		}
 		
 	}
 	
 	private void writeSequence() throws FileNotFoundException, IOException{
 		
-		write("sequenza");
+		writeOutputMessage("sequenza");
 		
 	}
 		
 	
-	private void write(String body) throws IOException{
-		
-		OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream(), Charset.forName("UTF-8"));
-		out.write("HTTP/1.1 200 OK\n");
-		out.write("Date: Tue, 17 Mar 2014 14:47:00\n");
-		out.write("Content-Type: text/html; charset=utf-8\n");
+	private void writeOutputMessage(String bodyMessage) throws IOException{
+		OutputStreamWriter out = initializeOutput();
+		out.write(bodyMessage);
 		out.write("\n");
-		out.write(body);
-		out.write("\n");
-		out.flush();
+		out.close();;
 	}
 	
 	
@@ -99,56 +131,47 @@ public class ThreadForSingleClientConnection extends Thread implements Runnable{
 		
 	}
 
-//
-//	private void writeOutput(OutputStreamWriter out)
-//			throws FileNotFoundException, IOException {
-//		BufferedReader fileReader=new BufferedReader(
-//			new FileReader("web/index01.html")
-//		);
-//		String fileLine=fileReader.readLine();
-//		while(fileLine!=null){
-//			out.write(fileLine+"\n");
-//			fileLine=fileReader.readLine();
-//		}
-//		fileReader.close();
-//		out.write("\n");
-//	}
-//
-//	private OutputStreamWriter initializeOutput() throws IOException {
-//		OutputStreamWriter out = new 
-//			OutputStreamWriter(
-//					clientSocket.getOutputStream(),
-//					Charset.forName("UTF-8").newEncoder()
-//			);
-//
-//		out.write("HTTP/1.1 200 OK\n");
-//		out.write("Date: Tue, 17 Mar 2014 14:47:00\n");
-//		out.write("Content-Type: text/html; charset=utf-8\n");
-//		out.write("\n");
-//		
-//		return out;
-//	}
-//
-//	private void getInputLines(LinkedList<String> lines) throws IOException {
-//		BufferedReader in = new BufferedReader(
-//				new InputStreamReader(
-//						clientSocket.getInputStream()
-//				)	
-//			);
-//			
-//			String line = in.readLine();
-//			
-//			
-//			while(line!=null){
-//				System.out.println(line);
-//				lines.add(line);
-//				line = in.readLine();
-//				if(line.length()==0){
-//					line=null;
-//				}
-//			}
-//			//in.close();
-//	}
+
+	private void writeOutputFromFile(String path) throws FileNotFoundException, IOException {
+		
+		BufferedReader fileReader;
+		try {
+			fileReader = new BufferedReader(new FileReader("web" + path));
+		} catch (FileNotFoundException e) {
+//			TODO: error
+			writeOutputMessage("file non trovato");
+			return;
+		}
+		
+		OutputStreamWriter out = initializeOutput();
+		
+		String fileLine=fileReader.readLine();
+		while(fileLine!=null){
+			out.write(fileLine+"\n");
+			fileLine=fileReader.readLine();
+		}
+		
+		fileReader.close();
+		out.write("\n");
+		out.close();
+	}
+
+	private OutputStreamWriter initializeOutput() throws IOException {
+		OutputStreamWriter out = new 
+			OutputStreamWriter(
+					clientSocket.getOutputStream(),
+					Charset.forName("UTF-8").newEncoder()
+			);
+
+		out.write("HTTP/1.1 200 OK\n");
+		out.write("Date: Tue, 17 Mar 2014 14:47:00\n");
+		out.write("Content-Type: text/html; charset=utf-8\n");
+		out.write("\n");
+		
+		return out;
+	}
+
+
 	
 }
 
