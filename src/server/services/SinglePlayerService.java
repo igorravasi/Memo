623e393f9854a33e6f771ed08b32283a5c7c3e0e
igorravasi <igorravasi@gmail.com>
2015-05-03 19:43:32 +0200

    Continuato refactoring, tolta al ServerThread la responsabilità di gestire il gioco singleplayer, e affidata alla nuova classe SinglePlayerService che implementa l'interfaccia IService

---
+++ src/server/services/SinglePlayerService.java
@@ -0,0 +1,68 @@
+package server.services;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import server.HttpRequest;
+import server.HttpStringMessage;
+import server.IService;
+import engine.SinglePlayerGame;
+
+public class SinglePlayerService implements IService {
+
+	private Map<Integer, SinglePlayerGame> singleGames = new HashMap<Integer, SinglePlayerGame>();
+	private HttpStringMessage message = new HttpStringMessage();
+	
+	
+	@Override
+	public void sendHttpResponse(Socket clientSocket, HttpRequest request)
+			throws IOException {
+		
+		String uri = request.getUri();
+		
+		if (!uri.contains("singleplayer/")) {	
+			initializeGame(clientSocket);
+		}else {
+			play(clientSocket, uri);
+		}
+
+	}
+
+	
+	
+	private void play(Socket clientSocket, String uri) throws IOException{
+		
+		Integer playId;
+		
+		int endOfPlayId = uri.indexOf('?');
+		if (endOfPlayId>0) {
+								
+			playId = Integer.parseInt(uri.substring("/singleplayer".length()+1,endOfPlayId));
+		}else {
+			playId = Integer.parseInt(uri.substring("/singleplayer".length()+1));
+		}
+		
+		SinglePlayerGame game = singleGames.get(playId);
+		String response = "E: No game Id";
+		if (game != null) {
+			response = game.readRequest(uri);
+		}
+		
+		message.sendMessage(clientSocket, response);
+		
+		
+	}
+	
+	
+	private void initializeGame(Socket clientSocket) throws IOException{
+		
+		//sostituire singleGames.size() con un numero calcolato appositamente, libero.
+		Integer playId = singleGames.size();
+		singleGames.put(playId, new SinglePlayerGame());
+		message.sendMessage(clientSocket, playId+"");
+	}
+	
+	
+}
