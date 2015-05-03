caa726e60f4b6cd7d785464e2b30877fcfb1665b
igorravasi <igorravasi@gmail.com>
2015-05-03 22:23:22 +0200

    Implementata la mappa dei servizi, associati a una determinata directory

--- src/server/ServerLife.java
+++ src/server/ServerLife.java
@@ -6,13 +6,14 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+import server.services.BinaryFileService;
 import server.services.SinglePlayerService;
+import server.services.TextFileService;
 
 public class ServerLife {
 
 	public static void main(String[] args) {
 		
-//		Map<Integer, SinglePlayerGame> singleGames = new HashMap<Integer, SinglePlayerGame>();
 		//TODO: Aggiungere un observer che osservi le notifiche dello status delle partite, che dovranno essere observable
 		
 		try {
@@ -22,15 +23,18 @@ public class ServerLife {
 			Socket clientSocket = null;
 			
 			Map<String, IService> services = new HashMap<String, IService>();
-//			services.put("/", new TextFileService());
+			services.put("/", new TextFileService());
+			services.put("/css", new TextFileService());
+			services.put("/js", new TextFileService());
+			services.put("/resources", new BinaryFileService());
+			services.put("/singleplayer", new SinglePlayerService());
 			
-			IService singleService = new SinglePlayerService();
 			
 			while (true) {
 				
 				try {
 					clientSocket = socket.accept();
-					new ServerThread(clientSocket, services,singleService).run();
+					new ServerThread(clientSocket, services).run();
 				} catch (Exception e) {
 					
 				}
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -6,17 +6,13 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
-import server.services.BinaryFileService;
-import server.services.TextFileService;
-
-//TODO: pesante refactoring. Questa classe è assolutamente troppo lunga e poco coesa!
 
 public class ServerThread extends Thread implements Runnable{
 
 	private Socket clientSocket;
 	
 	private Map<String, IService> services = new HashMap<String, IService>();
-	private IService singlesService;
+	
 	
 	
 	public ServerThread(Socket clientSocket,
@@ -24,15 +20,6 @@ public class ServerThread extends Thread implements Runnable{
 		super();
 		this.clientSocket = clientSocket;
 		this.services = services;
-	}
-	
-	
-	public ServerThread(Socket clientSocket,
-			 Map<String, IService> services, IService singleservice) {
-		super();
-		this.clientSocket = clientSocket;
-		this.services = services;
-		this.singlesService = singleservice;
 	}
 	
 
@@ -59,21 +46,21 @@ public class ServerThread extends Thread implements Runnable{
 			
 		String page = request.getUri();
 
+		IService service = services.get(page);
 		
-		if (page.contains("/singleplayer")) {
-			singlesService.sendHttpResponse(clientSocket, request);
-		} else {
-			if (page.endsWith(".jpeg")||page.endsWith(".jpg")||page.endsWith(".png")||page.endsWith(".ico")) {
-				new BinaryFileService().sendHttpResponse(clientSocket, request);
-			} else {
-				new TextFileService().sendHttpResponse(clientSocket, request);
+		while (service == null) {
+			
+			page = page.substring(0, page.lastIndexOf("/"));
+			if (page.equalsIgnoreCase("")) {
+				page = "/";
 			}
+			service = services.get(page);
 		}
 		
+		service.sendHttpResponse(clientSocket, request);
+			
 		
 	}
-	
-
 	
 	
 	
