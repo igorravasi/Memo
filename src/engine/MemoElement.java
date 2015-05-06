2696a3e6889adf54842b1d6b1774a5c297eb54c1
igorravasi <igorravasi@gmail.com>
2015-05-06 17:03:12 +0200

    -Commentati metodi non invocati attualmente. -I byte del codice utf delle emoticon vengono convertiti in stringa nell'esecuzione del metodo che la dovrebbe restituire

--- src/engine/MemoElement.java
+++ src/engine/MemoElement.java
@@ -6,15 +6,16 @@ import java.util.Random;
 public class MemoElement {
 
 	byte[] emojBytes = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x81};
-	String emoj = new String(emojBytes, Charset.forName("UTF-8"));
+	
 //	TODO: Rimpiazzare la logica MAX_INT_CODE e switch-case con un'enumerazione. 
 //	TODO: Refactoring della classe, possibili design pattern che migliorerebbero l'architettura e le prestazioni: flyweight e prototype 
 	
 	
-	public MemoElement(String emoj) {
-		super();
-		this.emoj = emoj;
-	}
+//	public MemoElement(byte lastByte) {
+//		super();
+//		this.emojBytes[emojBytes.length -1] = lastByte;
+//		
+//	}
 
 	public MemoElement() {
 		super();
@@ -28,11 +29,11 @@ public class MemoElement {
 	
 	
 	public String getEmoj() {
-		return emoj;
+		return new String(emojBytes, Charset.forName("UTF-8"));
 	}
 
-	public void setEmoj(String emoj) {
-		this.emoj = emoj;
-	}
+//	public void setEmoj(String emoj) {
+//		this.emoj = emoj;
+//	}
 	
 }
