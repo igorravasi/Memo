ccf8bcc01f9f7897f00ba59eadbfc2263e0e31e8
igorravasi <igorravasi@gmail.com>
2015-05-08 23:47:02 +0200

    Nella classe MemoElement:
    .)reimpostata la creazione casuale di un emoji
    .)aggiunto il metodo che restituisce le cifre esadecimali, separate da %, dei bytes dell'elemento
    .)pulizia commenti

--- src/engine/MemoElement.java
+++ src/engine/MemoElement.java
@@ -7,12 +7,12 @@ import java.util.Random;
 //TODO: Refactoring della classe, possibili design pattern che migliorerebbero l'architettura e le prestazioni: flyweight e prototype
 public class MemoElement {
 
-//	byte leftBound = (byte) 0x81;
-//	byte[] emojBytes = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, leftBound};
+	byte leftBound = (byte) 0x81;
+	byte[] emojBytes = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, leftBound};
 //	TODO: SCOMMENTARE prima di questo commento dopo ai test fatti con i digit dell'utf-8 anzichè le emoji
 //		e commentare le due righe dopo questo commento
-	byte leftBound = (byte)0x30;
-	byte[] emojBytes = {leftBound};
+//	byte leftBound = (byte)0x30;
+//	byte[] emojBytes = {leftBound};
 	
  
 	
@@ -38,6 +38,18 @@ public class MemoElement {
 	}
 
 	
+	public String getEscapedBytes(){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < emojBytes.length; i++) {
+			sb.append("%");
+			sb.append(String.format("%02X", emojBytes[i]));
+		}
+		
+		return sb.toString();
+	}
+	
 	private void randomizeLastByte(){
 		Random randomizer = new Random();
 		randomizer.setSeed(randomizer.nextLong());
@@ -47,8 +59,6 @@ public class MemoElement {
 
 	}
 	
-//	public void setEmoj(String emoj) {
-//		this.emoj = emoj;
-//	}
+
 	
 }
