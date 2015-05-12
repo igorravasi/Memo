546d04d10f63e40f256415e828fa671e92525a0d
igorravasi <igorravasi@gmail.com>
2015-05-12 18:40:14 +0200

    Modificato: testRegEx.java Annullato il supporto della regex a righe "miste" di parole e di "include", questo perchè 1 non era essenziale 2 semplifica il codice sorgente di inclusione

--- src/test/testRegEx.java
+++ src/test/testRegEx.java
@@ -4,53 +4,44 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 
 public class testRegEx {
 
 	public static void main(String[] args) {
-		String regEx = ".*\\*\\*\\* include: .+ \\*\\*\\*.*";
+//		String regEx = "\\#\\#\\#.+\\#\\#\\#";
+		String regEx = "#{3}.+#{3}";
 		System.out.println("Ecco la regex di prova : " + regEx);
 	
 		List<String> stringhe = new LinkedList<String>();
 		Collections.addAll(stringhe, 
 				"giro giro tondo",
 				"casca il mondo",
-				"casca la terra",
-				"casca la *** include: terra *** tutti giù per terra",
-				"*** include: memo.txt ***",
+				" casca la terra ",
+				"NON PIù SUPPORTATO casca la ### include: terra ### tutti giù per terra",
+				"### memo.txt ###",
 				"ahahhaha"
 				);
 		
 	for (Iterator<String> iterator = stringhe.iterator(); iterator.hasNext();) {
 		String string = (String) iterator.next();
 		if (string.matches(regEx)) {
-			System.out.print("GOOD: ");
+			System.out.print("GOOD: filename is");
+			String path =  new StringTokenizer(string,"###").nextToken();
+			
+			System.out.println(path);
+			
 		}
-		System.out.println(string);
+		System.out.println(string.trim());
 		
 	}
+
+
 	
 	
-	for (Iterator<String> iterator = stringhe.iterator(); iterator.hasNext();) {
-		String string = (String) iterator.next();
-		if (string.matches(regEx)) {
-			String resultLine ="";
-			int startPos = string.indexOf("*** include: ");
-			
-			resultLine += string.substring(0, startPos);
-			string = string.substring(startPos + "*** include: ".length());
-			
-			int endPos = string.indexOf(" ***");
-			String filename = "/web/includes/" + string.substring(0, endPos); 
-			resultLine +=filename;
-			resultLine += string.substring(endPos + " ***".length());
-			
-			System.err.println(resultLine);
-		} 
-	}
 	
-	
+
 	}
 	
 	
