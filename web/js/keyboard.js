f68c0a8b3ff0cba5de2bdf04620fafc7b2181078
ghia16 <ghia16@hotmail.it>
2015-05-12 11:14:52 +0200

    Modificato: keyboard.js Ripristinata tastiera a versione precedente

--- web/js/keyboard.js
+++ web/js/keyboard.js
@@ -1,4 +1,4 @@
-﻿/**
+/*
  * EmojiKeyboardPrinter
  */
 
@@ -7,30 +7,24 @@ function keyboard(emoji){
 	document.getElementById("sequenza").value += emoji;
 	}
 
-function writeTheKeyboard() {
-	//TODO: In lontano futuro: supporto anche ai browser che non supportano emoji (in quel caso vanno caricate come immagini)
-	var baseCodePoint = "😄".codePointAt(0);
-//	var baseCodePoint = "😄".charCodeAt(0);
-		
-	var keyboard = "";
-	for (var int = 0; int < 10; int++) {
-		var code = baseCodePoint + int;
-		var emoji = String.fromCodePoint(code)+"";
-		keyboard += '<button class="emoji" onclick="keyboard(\'' + emoji + '\');">' + emoji + '</button>';
-	}
-//	keyboard ="";
-//	for (var int = 120000; int < 130000; int++) {
-//		keyboard += int + ": " + String.fromCharCode(int) + "\n";
-//	}
-//	
-	document.getElementById("emojikeyboard").innerHTML=keyboard;
+function writeTheKeyboard(content) {
+	document.getElementById("emojikeyboard").innerHTML=content;
 }
 
 function loadKeyboard(){
 	
 	//TODO: If (tastiera non c'è ancora) then scrivila altrimenti, se c'è ed è nascosta mostrala.
 	//Nasconderla dopo l'invio
-	
-	writeTheKeyboard();
+	writeTheKeyboard(''+
+	'<button class="emoji" onclick="keyboard(\'😄\');">😄</button>'+
+	'<button class="emoji" onclick="keyboard(\'😆\');">😆</button>'+
+	'<button class="emoji" onclick="keyboard(\'😉\');">😉</button>'+
+	'<button class="emoji" onclick="keyboard(\'😊\');">😊</button>'+
+	'<button class="emoji" onclick="keyboard(\'😃\');">😃</button>'+
+	'<button class="emoji" onclick="keyboard(\'😁\');">😁</button>'+
+	'<button class="emoji" onclick="keyboard(\'😅\');">😅</button>'+
+	'<button class="emoji" onclick="keyboard(\'😂\');">😂</button>'+
+	'<button class="emoji" onclick="keyboard(\'😇\');">😇</button>'+
+	'<button class="emoji" onclick="keyboard(\'😈\');">😈</button>')
 	
 }
