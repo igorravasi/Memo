56ff12b0d138b9c4f493da034f8b8083401ad96b
igorravasi <igorravasi@gmail.com>
2015-05-15 16:46:31 +0200

    Aggiornamento: Modificato: HTTPRequest.java

--- src/server/basics/HttpRequest.java
+++ src/server/basics/HttpRequest.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class HttpRequest {
 
 	private String uri;
-
+	private LinkedList<String> lines;
+	
 	public HttpRequest(final Socket clientSocket) throws IOException{
 		super();
 		BufferedReader in = new BufferedReader(
@@ -23,7 +25,7 @@ public class HttpRequest {
 		
 		//L'uri è nella prima riga, al secondo posto.
 		String uri=lineElements[1];
-		LinkedList<String> lines=new LinkedList<String>();
+		lines=new LinkedList<String>();
 		while(line!=null){
 			
 			lines.add(line);
@@ -33,11 +35,23 @@ public class HttpRequest {
 			}
 		}
 		this.uri=uri;
+		
+		
+		
+		//TODO: rimuovere dopo al debug
+		for (Iterator iterator = lines.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			System.err.println(string);
+		}
+		
 	}
 	
 	public String getUri() {
 		return uri;
 	}
 	
+	public LinkedList<String> getLines(){
+		return lines;
+	}
 
 }
