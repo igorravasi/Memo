55b7b2623547fa171ee6bed7a622fc18661517d3
igorravasi <igorravasi@gmail.com>
2015-05-16 21:27:53 +0200

    Aggiornato:Modificato: HttpRequest.java . La classe recupera il Content-Length della richiesta e legge il contenuto, recuperabile con il metodo getContent()

--- src/server/basics/HttpRequest.java
+++ src/server/basics/HttpRequest.java
@@ -4,45 +4,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpRequest {
 
 	private String uri;
-	private LinkedList<String> lines;
+	
+	private Map<String, String> headers = new HashMap<String, String>();
+	private String content = null;
 	
 	public HttpRequest(final Socket clientSocket) throws IOException{
 		super();
-		BufferedReader in = new BufferedReader(
-			new InputStreamReader(
-					clientSocket.getInputStream()
-			)	
-		);
-		String line = in.readLine();
+		BufferedReader in = new BufferedReader(	new InputStreamReader( clientSocket.getInputStream() ) );
 		
+		String line = in.readLine();
 		String[] lineElements=line.split(" ");
 		
 		//L'uri è nella prima riga, al secondo posto.
+		
+		boolean isPost = lineElements[0].equalsIgnoreCase("POST") ? true : false;
+		
 		String uri=lineElements[1];
-		lines=new LinkedList<String>();
-		while(line!=null){
+		
+
+		headers.put("Request:", line);
+
+		
+		while(line!=null && line.length() != 0){
+			String[] headerElements = line.split(" ", 2);
+			headers.put(headerElements[0], headerElements[1]);
 			
-			lines.add(line);
 			line = in.readLine();
-			if(line.length()==0){
-				line=null;
+		}
+		
+		if (isPost) {
+			
+			int contentLength;
+			
+			try {
+				contentLength = Integer.parseInt( headers.get("Content-Length:") );
+			} catch (NumberFormatException e) {
+				contentLength = 0;
 			}
+			
+			int c = 0;
+			content = "";
+			
+			for (int i = 0; i < contentLength; i++) {
+				c = in.read();
+				content += (char) c;
+			}
+			
 		}
+		
 		this.uri=uri;
-		
-		
-		
-		//TODO: rimuovere dopo al debug
-		for (Iterator iterator = lines.iterator(); iterator.hasNext();) {
-			String string = (String) iterator.next();
-			System.err.println(string);
-		}
+	
 		
 	}
 	
@@ -50,8 +67,8 @@ public class HttpRequest {
 		return uri;
 	}
 	
-	public LinkedList<String> getLines(){
-		return lines;
+	public String getContent(){
+		return content;
 	}
 
 }
