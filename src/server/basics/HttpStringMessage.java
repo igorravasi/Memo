ef7fcbb1d066b4934dfc423703ee97a7ebae2119
igorravasi <igorravasi@gmail.com>
2015-05-23 23:50:31 +0200

    Refactoring:Modificato: HttpStringMessage.java . Anzichè sfruttare un'istanza interna di HttpMessage la classe HttpStringMessage ora estende HttpMessage ereditandone tutti i metodi e estendendone le funzionalità.
    (Non è piu un facade)

--- src/server/basics/HttpStringMessage.java
+++ src/server/basics/HttpStringMessage.java
@@ -3,16 +3,14 @@ package server.basics;
 import java.io.IOException;
 import java.net.Socket;
 
-public class HttpStringMessage {
-
-	HttpMessage message = new HttpMessage();
+public class HttpStringMessage extends HttpMessage{
 	
 	public void sendMessage(Socket clientSocket, String messageBody) throws IOException{
 		
-		message.setContentType("text/html; charset=utf-8");
-		message.sendResponseHeader(clientSocket);
-		message.getOut().write(messageBody);
-		message.closeHttpResponse();
+		setContentType("text/html; charset=utf-8");
+		sendResponseHeader(clientSocket);
+		getOut().write(messageBody);
+		closeHttpResponse();
 	}
 	
 }
