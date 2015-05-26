270c3a1c42693cad2e57bd26c3b9fdf1ba318c97
igorravasi <igorravasi@gmail.com>
2015-05-26 15:14:50 +0200

    Refactoring: Aggiunto: scriptsForGame.js e adattamenti conseguenti nelle altre pagine. Piccole modifiche

--- web/js/SinglePlayer.js
+++ web/js/SinglePlayer.js
@@ -3,7 +3,6 @@
  */
 
 var playId = null;
-//var xmlHttp = null;
 var intervalTimer;
 var timeoutTimer = null;
 var factorySeconds = 7;
@@ -53,36 +52,6 @@ function doTheRightThing(response) {
 }
 
 
-
-function showSequence(sequence) {
-	
-	document.getElementById("sequence").innerHTML=sequence;
-	display("sequence_container",true);
-	
-	
-	var timerText = document.getElementById("countDown");
-	timerText.innerHTML = factorySeconds;
-	
-	intervalTimer = setInterval(function() {
-
-		if (document.getElementById("countDown")) {
-			if (timerText.innerHTML > 1) {
-				
-				timerText.innerHTML = timerText.innerHTML -1;
-			}else {
-				window.clearInterval(intervalTimer);
-				var numberOfChars = utf_length( trim(sequence) );
-				readSequence(numberOfChars);
-				
-			}
-		} else {
-			window.clearInterval(intervalTimer);
-			
-		}
-		
-	},1000);
-}
-
 function validate(){
 	
 	
@@ -116,48 +85,6 @@ function readSequence(numberOfChars){
 }
 
 
-
-function generateBoxes(n){
-	
-	
-	if (document.getElementById("box_container")) {
-
-		document.getElementById("box_container").parentNode.removeChild(document.getElementById("box_container"));
-	}
-	
-	
-	var boxContainer = document.createElement("div");
-	var controls = document.getElementById("controls");
-	boxContainer.setAttribute("id","box_container");
-	
-	controls.insertBefore(boxContainer,controls.childNodes[0]);
-
-	
-	for (var i = 0; i < n; i++) {
-		
-		var input = document.createElement("input");
-		input.setAttribute('type',"text");
-		input.setAttribute('class',"input-emoji");
-		input.setAttribute('id', "box"+i);
-		input.setAttribute('size', "1");
-		input.setAttribute('readonly',"readonly");
-		input.setAttribute('onclick',"lastBox="+i);
-		boxContainer.appendChild(input);
-		
-	}
-	
-	lastBox = 0;
-	
-}
-
-
-
-function writeAMessage(content) {
-	document.getElementById("message").innerHTML=content;
-	display("sequence_container",false);
-	display("controls", false)
-	display("message",true);
-}
 
 function keyboard(emoji){
 	
--- web/js/graphicLib.js
+++ web/js/graphicLib.js
@@ -15,3 +15,77 @@ function display(elementId, boolDisplay){
 	element.className = elementClasses;
 		
 }
+
+
+
+
+function showSequence(sequence) {
+	
+	document.getElementById("sequence").innerHTML=sequence;
+	display("sequence_container",true);
+	
+	
+	var timerText = document.getElementById("countDown");
+	timerText.innerHTML = factorySeconds;
+	
+	intervalTimer = setInterval(function() {
+
+		if (document.getElementById("countDown")) {
+			if (timerText.innerHTML > 1) {
+				
+				timerText.innerHTML = timerText.innerHTML -1;
+			}else {
+				window.clearInterval(intervalTimer);
+				var numberOfChars = utf_length( trim(sequence) );
+				readSequence(numberOfChars);
+				
+			}
+		} else {
+			window.clearInterval(intervalTimer);
+			
+		}
+		
+	},1000);
+}
+
+
+function generateBoxes(n){
+	
+	
+	if (document.getElementById("box_container")) {
+
+		document.getElementById("box_container").parentNode.removeChild(document.getElementById("box_container"));
+	}
+	
+	
+	var boxContainer = document.createElement("div");
+	var controls = document.getElementById("controls");
+	boxContainer.setAttribute("id","box_container");
+	
+	controls.insertBefore(boxContainer,controls.childNodes[0]);
+
+	
+	for (var i = 0; i < n; i++) {
+		
+		var input = document.createElement("input");
+		input.setAttribute('type',"text");
+		input.setAttribute('class',"input-emoji");
+		input.setAttribute('id', "box"+i);
+		input.setAttribute('size', "1");
+		input.setAttribute('readonly',"readonly");
+		input.setAttribute('onclick',"lastBox="+i);
+		boxContainer.appendChild(input);
+		
+	}
+	
+	lastBox = 0;
+	
+}
+
+function writeAMessage(content) {
+	document.getElementById("message").innerHTML=content;
+	display("sequence_container",false);
+	display("controls", false)
+	display("message",true);
+}
+
