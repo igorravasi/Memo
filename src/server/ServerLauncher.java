c5bbbd7b9601ecf21ec6322d89589040f416d0de
igorravasi <igorravasi@gmail.com>
2015-05-29 12:35:18 +0200

    Aggiornato: Modificato: MemoServerConfigurator.java , ServerLauncher.java . Il configurator carica i parametri da opportuni files. ServerLauncher non comunica più i content types (vengono letti da configurator) Aggiunto: content-types.cfg , values.cfg (files di configurazione)

--- src/server/ServerLauncher.java
+++ src/server/ServerLauncher.java
@@ -1,6 +1,5 @@
 package server;
 
-import server.config.MemoServerConfigurator;
 import server.services.BinaryFileService;
 import server.services.LoggingService;
 import server.services.SinglePlayerService;
@@ -16,9 +15,6 @@ public class ServerLauncher {
 		
 		TextFileService fileService = new TextFileService();
 		BinaryFileService binaryService = new BinaryFileService();
-		
-	
-		addContentTypes();
 
 
 		server.addService("/", fileService);
@@ -39,22 +35,7 @@ public class ServerLauncher {
 	
 //TODO:	
 //	public static void generateKeyboardFile(){
-//	
 //		
 //	}
-	
-	
-	public static void addContentTypes(){
-		
-		MemoServerConfigurator configurator = MemoServerConfigurator.getInstance();
 
-		configurator.addContentType(".html", "text/html");
-		configurator.addContentType(".css", "text/css");
-		configurator.addContentType(".js", "text/javascript");
-		configurator.addContentType(".jpg", "image/jpeg");
-		configurator.addContentType(".jpeg", "image/jpeg");
-		configurator.addContentType(".png", "image/jpeg");
-		configurator.addContentType(".bmp", "image/jpeg");
-		configurator.addContentType("ico", "image/ico");
-	}
 }
--- src/server/config/MemoServerConfigurator.java
+++ src/server/config/MemoServerConfigurator.java
@@ -1,18 +1,31 @@
 package server.config;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class MemoServerConfigurator {
 
-	//Singleton
+	private static final String dir = "config/";
+	private static final String valuesPath = dir + "values.cfg";
+	private static final String contentTypesPath = dir + "content-types.cfg";
+	
+	private static final String delim = ":/t";
+	
 	private static MemoServerConfigurator  configurator = null;
-	
 	private Map<String, String> contentTypes = new HashMap<String, String>();
+	private Map<String, String> values = new HashMap<String, String>();
 	
+	
+	//Singleton: costruttore private
 	private MemoServerConfigurator(){
 		
 	}
+	
 	
 	public static MemoServerConfigurator getInstance(){
 		if (configurator == null) {
@@ -22,14 +35,55 @@ public class MemoServerConfigurator {
 		return configurator;
 	}
 	
-	
-	public String addContentType(String extension, String contentType){
-		return contentTypes.put(extension, contentType);
+	private void reload(String path, Map<String, String> mappa){
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			
+			String line = reader.readLine();
+			
+			while (line != null) {
+				StringTokenizer tokenizer = new StringTokenizer(line, delim);
+				
+				String key = tokenizer.nextToken();
+				String value = tokenizer.nextToken("");
+				
+				mappa.put(key, value);
+				
+				line = reader.readLine();
+			}
+			
+			reader.close();
+		}
+		
+		//Non faccio nulla per gestire l'eccezione, al di là del log, perchè la miglior cosa da fare è lasciare
+		//i valori cosi come erano prima della chiamata 
+		  catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void reloadContentTypes(){
+		//TODO: check ultima modifica
+		reload(contentTypesPath, contentTypes);
+	}
+
+	private void reloadValues(){
+		
+		//TODO: check ultima modifica
+		
+		reload(valuesPath, values);
+	}
 	public String getContentType(String extension){
+		reloadContentTypes();
 		return contentTypes.get(extension);
 	}
 	
-	//TODO: caricare da file di testo (check dell'ultima modifica)
+	public String getValue(String name){
+		reloadValues();
+		return values.get(name);
+	}
+
 }
