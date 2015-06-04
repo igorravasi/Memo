f746847a60bb089bb4644d05c6c7a6bc1aa42476
igorravasi <igorravasi@gmail.com>
2015-06-04 17:16:54 +0200

    Modificato: MultiPlayerService.java . Aggiunto controllo di validità della sessione

--- src/server/services/MultiPlayerService.java
+++ src/server/services/MultiPlayerService.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.net.Socket;
 
 import server.basics.HttpRequest;
+import server.basics.HttpStringMessage;
 import server.services.extensions.SessionManager;
 
 public class MultiPlayerService implements IService {
 
 	private SessionManager sessionManager;
+	private HttpStringMessage message = new HttpStringMessage();
 	
 	public MultiPlayerService(SessionManager sessionManager) {
 
@@ -17,7 +19,33 @@ public class MultiPlayerService implements IService {
 	@Override
 	public void sendHttpResponse(Socket clientSocket, HttpRequest request) throws IOException {
 		
+		
+		if (!validRequest(request)) {
+			message.sendMessage(clientSocket, "E:Relog");
+		} else {
+			message.sendMessage(clientSocket, "TUTTOK");
+		}
 
+		
 	}
+	
+	private boolean validRequest ( HttpRequest request ){
+		String user = request.getCookies().get("Utente");
+		Long sessionId;
+		
+		try {
+			sessionId = Long.parseLong(request.getCookies().get("Sessione"));
+		} catch (NumberFormatException e) {
+			sessionId = 0L;
+		}
+		
+		if (user != null && sessionManager.checkSession(sessionId, user)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
 
 }
