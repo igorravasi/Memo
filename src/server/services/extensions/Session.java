b2181b9007876c79c91f90d572e05775fa312a69
igorravasi <igorravasi@gmail.com>
2015-06-04 11:25:26 +0200

    Modificato: Session.java . Estende observable: notifica gli observable quando è passato un determinato tempo (e la sessione deve scadere). Aggiunto il metodo stopTimer che permette di cancellare il timer per la sscadenza

--- src/server/services/extensions/Session.java
+++ src/server/services/extensions/Session.java
@@ -1,15 +1,27 @@
 package server.services.extensions;
 
-public class Session {
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Session extends Observable{
 	
 	
 //	private String user;
 	private Long sessionId;
-	
+	private Timer expire = new Timer();
 	
 	public Session(Long sessionId, String user){
 //		this.user = user;
 		this.sessionId = sessionId;
+		this.expire.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				setChanged();
+				notifyObservers();	
+			}
+		}, 1000*60*60);
 		
 	}
 	
@@ -17,6 +29,9 @@ public class Session {
 		return sessionId;
 	}
 	
+	public void stopTimer(){
+		this.expire.cancel();
+	}
 //	public String getUser(){
 //		return user;
 //	}
