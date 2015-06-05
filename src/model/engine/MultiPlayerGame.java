cadb6274834397eb446ab0bcddc8a32348a4d27a
igorravasi <igorravasi@gmail.com>
2015-06-05 11:20:44 +0200

    Modificato: MultiPlayerGame.java . Aggiunto metodo per controllare se il gioco è già iniziato

--- src/model/engine/MultiPlayerGame.java
+++ src/model/engine/MultiPlayerGame.java
@@ -13,12 +13,14 @@ import java.util.TimerTask;
 public class MultiPlayerGame extends Observable{
 
 	private static final int LOOSER = -1;
-	private static final int ALIVE_TIMER = 1000*60*2; //DUE MINUTI
+	private static final int ALIVE_TIMER = 1000*60*20; //VENTI MINUTI PER GIOCARE LA PARTITA INTERA
 	private static final String SEQUENCE_ID ="S:";
 	
 	private MemoSequence sequence = new MemoSequence();
 	private Integer round = 0;
 	private Timer isAliveTimer = new Timer();
+	private String gamers[] = {null, null};
+	
 	private Integer playId;
 	
 	public Integer getPlayId() {
@@ -38,70 +40,80 @@ public class MultiPlayerGame extends Observable{
 		
 		isAliveTimer.schedule(new TimerTask() {
 			
-			/* (non-Javadoc)
-			 * @see java.util.TimerTask#run()
-			 */
+			
 			@Override
 			public void run() {
 				
+				//TODO: decretare il vincitore
 				changed();
 				
 			}
-		}, ALIVE_TIMER); //Dopo due minuti di inattivita' notifica l'observer
+		}, ALIVE_TIMER); 
 	}
 	
-	private int playerMoved(String playerSequence){
-		keepAlive();
-		playerSequence = playerSequence.substring(playerSequence.indexOf(SEQUENCE_ID)+SEQUENCE_ID.length());
-		playerSequence = playerSequence.trim();
-				
-		String actualSequence = getEscapedSequence();
-			
-		if (actualSequence.equalsIgnoreCase(playerSequence)) {
-			round++;
-			sequence.nextRound();
-			return round;
-		}else {
-			
-			return LOOSER;
+	
+	public boolean isStarted(){
+
+		for (int i = 0; i < gamers.length; i++) {
+			if (gamers[i] == null) {
+				return false;
+			}
 		}
 		
+		return true;
 		
 	}
 	
-	/**
-	 * Leggo la sequenza inserita dal giocatore
-	 * @param content
-	 * @return nuova sequenza  o risultato o errore
-	 */
-	public String readRequest(String content){
-		
-		if (round == 0) {
-			keepAlive();
-			round++;
-			return "S:"+ getSequence();
-		} else {
-			
-			if (content.indexOf(SEQUENCE_ID) >= 0) {
-				
-				return getTheResult(playerMoved(content));
-			} else {
-				return "E: Malformed request, Reload the page";
-			}	
-		}
-		
-	}
+
+//	private int playerMoved(String playerSequence){
+//		keepAlive();
+//		playerSequence = playerSequence.substring(playerSequence.indexOf(SEQUENCE_ID)+SEQUENCE_ID.length());
+//		playerSequence = playerSequence.trim();
+//				
+//		String actualSequence = getEscapedSequence();
+//			
+//		if (actualSequence.equalsIgnoreCase(playerSequence)) {
+//			round++;
+//			sequence.nextRound();
+//			return round;
+//		}else {
+//			
+//			return LOOSER;
+//		}
+//		
+//		
+//	}
 	
-	private String getTheResult(int roundResult){
-		
-		if (roundResult == LOOSER) {
-			changed();
-			return "L: "+ round;
-		} else {
-			return SEQUENCE_ID + getSequence();
-		}
-			
-	}
+
+
+//	public String readRequest(String content){
+//		
+//		if (round == 0) {
+//			keepAlive();
+//			round++;
+//			return "S:"+ getSequence();
+//		} else {
+//			
+//			if (content.indexOf(SEQUENCE_ID) >= 0) {
+//				
+//				return getTheResult(playerMoved(content));
+//			} else {
+//				return "E: Malformed request, Reload the page";
+//			}	
+//		}
+//		
+//	}
+	
+//	private String getTheResult(int roundResult){
+//		
+//		if (roundResult == LOOSER) {
+//			changed();
+//			return "L: "+ round;
+//		} else {
+//			return SEQUENCE_ID + getSequence();
+//		}
+//			
+//	}
 
 
 	private void changed(){
@@ -109,14 +121,14 @@ public class MultiPlayerGame extends Observable{
 		notifyObservers();
 	}
 	
-	private String getSequence(){
-		return sequence.toString();
-	
-	}
-	
-	private String getEscapedSequence(){
-		return sequence.getEscapedString();
-	}
+//	private String getSequence(){
+//		return sequence.toString();
+//	
+//	}
+//	
+//	private String getEscapedSequence(){
+//		return sequence.getEscapedString();
+//	}
 
 
 }
