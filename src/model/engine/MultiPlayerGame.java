4defa69a3c85c6b06740317297f49d151af3423a
igorravasi <igorravasi@gmail.com>
2015-06-05 10:40:51 +0200

    Aggiunto: MultiPlayerGame.java . Classe che rappresenta un singolo gioco multiPlayer

---
+++ src/model/engine/MultiPlayerGame.java
@@ -0,0 +1,122 @@
+package model.engine;
+
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Classe per gestire il gioco singolo.
+ * Inizializzo i valori 
+ * ex. imposto il timer di due minuti per inserire la sequenza
+ *
+ */
+public class MultiPlayerGame extends Observable{
+
+	private static final int LOOSER = -1;
+	private static final int ALIVE_TIMER = 1000*60*2; //DUE MINUTI
+	private static final String SEQUENCE_ID ="S:";
+	
+	private MemoSequence sequence = new MemoSequence();
+	private Integer round = 0;
+	private Timer isAliveTimer = new Timer();
+	private Integer playId;
+	
+	public Integer getPlayId() {
+		return playId;
+	}
+
+	public MultiPlayerGame(Integer playId) {
+		super();
+		this.playId = playId;
+		
+		keepAlive();
+	}
+
+	private void keepAlive(){
+		isAliveTimer.cancel();
+		isAliveTimer = new Timer();
+		
+		isAliveTimer.schedule(new TimerTask() {
+			
+			/* (non-Javadoc)
+			 * @see java.util.TimerTask#run()
+			 */
+			@Override
+			public void run() {
+				
+				changed();
+				
+			}
+		}, ALIVE_TIMER); //Dopo due minuti di inattivita' notifica l'observer
+	}
+	
+	private int playerMoved(String playerSequence){
+		keepAlive();
+		playerSequence = playerSequence.substring(playerSequence.indexOf(SEQUENCE_ID)+SEQUENCE_ID.length());
+		playerSequence = playerSequence.trim();
+				
+		String actualSequence = getEscapedSequence();
+			
+		if (actualSequence.equalsIgnoreCase(playerSequence)) {
+			round++;
+			sequence.nextRound();
+			return round;
+		}else {
+			
+			return LOOSER;
+		}
+		
+		
+	}
+	
+	/**
+	 * Leggo la sequenza inserita dal giocatore
+	 * @param content
+	 * @return nuova sequenza  o risultato o errore
+	 */
+	public String readRequest(String content){
+		
+		if (round == 0) {
+			keepAlive();
+			round++;
+			return "S:"+ getSequence();
+		} else {
+			
+			if (content.indexOf(SEQUENCE_ID) >= 0) {
+				
+				return getTheResult(playerMoved(content));
+			} else {
+				return "E: Malformed request, Reload the page";
+			}	
+		}
+		
+	}
+	
+	private String getTheResult(int roundResult){
+		
+		if (roundResult == LOOSER) {
+			changed();
+			return "L: "+ round;
+		} else {
+			return SEQUENCE_ID + getSequence();
+		}
+			
+	}
+
+
+	private void changed(){
+		setChanged();
+		notifyObservers();
+	}
+	
+	private String getSequence(){
+		return sequence.toString();
+	
+	}
+	
+	private String getEscapedSequence(){
+		return sequence.getEscapedString();
+	}
+
+
+}
