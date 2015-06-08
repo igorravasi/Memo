a42b6c11844550e6c4628557ea3721f4e3ff78b6
igorravasi <igorravasi@gmail.com>
2015-06-08 15:23:51 +0200

    Modificato: MemoSequenceTest.java . Modificato test per testare MemoSequence con i nuovi metodi

--- src/test/MemoSequenceTest.java
+++ src/test/MemoSequenceTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import javax.sound.midi.Sequence;
+
 import model.engine.MemoSequence;
 /**
  * Verifico l'incremento di 2 elementi per ogni serie, 5 sequenze in questo caso.
@@ -14,12 +16,17 @@ public class MemoSequenceTest {
 		
 		System.out.println(myMemoSequence);
 	
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 1; i++) {
 			myMemoSequence.nextRound();
 			
 			System.out.println(myMemoSequence);
 
 		}
+		
+		System.err.println(myMemoSequence.getEscapedString());
+		System.err.println(myMemoSequence.getEscapedString(0));
+		System.err.println(myMemoSequence.getEscapedString(1));
+		
 	}
 
 }
